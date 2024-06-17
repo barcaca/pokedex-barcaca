f12f55b101462065703b2bbf482085018ff34e9d
barcaca <73846225+barcaca@users.noreply.github.com>
2024-06-17 19:24:30 -0300

    docs: atualiza documentação no header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -4,6 +4,10 @@ import { Icons } from '../icons'
 import { ThemeToggle } from '../theme/theme-toggle'
 import { Badge } from '../ui/badge'
 
+/**
+ * An array of objects representing the technologies used in the application.
+ * Each object contains a title (name of the technology) and an icon (React component).
+ */
 const buildItems = [
   {
     title: 'React',
@@ -27,16 +31,31 @@ const buildItems = [
   },
 ]
 
+/**
+ * Header component for the application.
+ * Displays the theme toggle, logo, description, and a list of technologies used.
+ *
+ * @returns {React.FC} - A functional component representing the Header.
+ */
 export function Header() {
   return (
     <header className="p-3 md:p-4">
       <div className="mx-auto flex max-w-7xl flex-col items-center gap-3">
+        {/* Theme toggle component */}
         <ThemeToggle />
+
+        {/* Application logo */}
         <Image width={250} height={50} src="/pokemon-logo.svg" alt="" />
+
+        {/* Application description */}
         <p className="text-center text-muted-foreground">
           Unofficial representation of a pokedex using API called PokeApi
         </p>
-        <p className="text-muted-foreground">Build with</p>
+
+        {/* Text indicating the technologies used */}
+        <p className="text-muted-foreground">Built with</p>
+
+        {/* List of badges for each technology used */}
         <div className="flex gap-2">
           {buildItems.map((item) => {
             return (
