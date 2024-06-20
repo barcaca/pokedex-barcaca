8e1bddf33e2d45ea6eb719f542d5140569c181ef
barcaca <73846225+barcaca@users.noreply.github.com>
2024-06-20 19:59:23 -0300

    chore: remove tipos desnecessários da aplicação

--- src/types/pokemon.ts
+++ src/types/pokemon.ts
@@ -40,19 +40,7 @@ export interface IPokemonSprites {
   front_default: string | null
   /** The shiny depiction of this Pokémon from the front in battle */
   front_shiny: string | null
-  /** The female depiction of this Pokémon from the front in battle */
-  front_female: string | null
-  /** The shiny female depiction of this Pokémon from the front in battle */
-  front_shiny_female: string | null
-  /** The default depiction of this Pokémon from the back in battle */
-  back_default: string | null
-  /** The shiny depiction of this Pokémon from the back in battle */
-  back_shiny: string | null
-  /** The female depiction of this Pokémon from the back in battle */
-  back_female: string | null
-  /** The shiny female depiction of this Pokémon from the back in battle */
-  back_shiny_female: string | null
-  /** Dream World, Official Artwork and Home sprites */
+  /** Official Artwork sprites */
   other?: OtherPokemonSprites
 }
 
