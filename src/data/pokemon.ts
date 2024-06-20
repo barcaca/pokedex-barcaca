c925ba488af687a893ede601be8f55421fb094e1
barcaca <73846225+barcaca@users.noreply.github.com>
2024-06-20 20:00:01 -0300

    feat: adiciona funções utils e dados para chamadas de API

--- src/data/pokemon.ts
+++ src/data/pokemon.ts
@@ -1,26 +1,98 @@
 import { cache } from 'react'
 
-import { APIResourceList } from '@/types/pokemon'
+import { APIResourceList, IPokemon, IPokemonSpecies } from '@/types/pokemon'
+import {
+  IChainLink,
+  IEvolution,
+  IEvolutionChain,
+} from '@/types/pokemon-evolution'
+import { IType } from '@/types/pokemon-types'
 
 import { BASE_URL, ENDPOINTS } from './base'
 
 /**
  * List Pokemons
- * @param offset The first item that you will get
- * @param limit How many Pokemons Stats per page
- * @returns A list of Pokemons
+ * @param page - The page number to fetch.
+ * @param limit - The number of Pokemons to fetch per page.
+ * @param pokemon - (Optional) The name of a specific Pokemon to fetch.
+ * @returns A Promise that resolves to the fetched list of Pokemons.
  */
 async function fetchListPokemons(
   page: number,
   limit: number,
   pokemon?: string,
 ): Promise<APIResourceList> {
-  const calculatedOffset = page > 1 ? (page - 1) * 10 : 0
+  // Calculate the offset, limiting it to a maximum of 1025
+  const calculatedOffset = Math.min((page - 1) * limit, 1025)
+
+  // Calculate the number of Pokemons to fetch, limiting it to a maximum of 1025 - calculatedOffset
+  const count = Math.min(limit, 1025 - calculatedOffset)
 
   const response = await fetch(
-    `${BASE_URL.REST}${ENDPOINTS.POKEMON}?limit=${limit}&offset=${calculatedOffset}`,
+    `${BASE_URL.REST}${ENDPOINTS.POKEMON}?limit=${count}&offset=${calculatedOffset}`,
   )
   const data: APIResourceList = await response.json()
   return data
 }
 export const getListPokemons = cache(fetchListPokemons)
+
+/**
+ * Fetches a single Pokemon by its name.
+ * @param name - The name of the Pokemon to fetch.
+ * @returns A Promise that resolves to the fetched Pokemon data.
+ */
+async function fetchPokemon(name: string): Promise<IPokemon> {
+  const response = await fetch(`${BASE_URL.REST}${ENDPOINTS.POKEMON}/${name}`)
+  const data: IPokemon = await response.json()
+  return data
+}
+
+export const getPokemon = cache(fetchPokemon)
+
+async function fetchPokemonSpecies(id: number): Promise<IPokemonSpecies> {
+  const response = await fetch(`${BASE_URL.REST}${ENDPOINTS.SPECIES}/${id}`)
+  const data: IPokemonSpecies = await response.json()
+  return data
+}
+
+export const getPokemonSpecies = cache(fetchPokemonSpecies)
+
+async function fetchType(type: string): Promise<IType> {
+  const response = await fetch(`${BASE_URL.REST}${ENDPOINTS.TYPE}/${type}`)
+  const data: IType = await response.json()
+  return data
+}
+
+export const getType = cache(fetchType)
+
+async function fetchEvolutions(url: string): Promise<IEvolution[]> {
+  const evolutions: IEvolution[] = []
+  const parts = url.split('/')
+  const id = Number(parts[parts.length - 2])
+
+  const response = await fetch(`${BASE_URL.REST}${ENDPOINTS.EVOLUTION}/${id}`)
+  const data: IEvolutionChain = await response.json()
+
+  async function traverseEvolutions(chain: IChainLink) {
+    const speciesName = chain.species.name
+    const pokemonDetails: IPokemon = await getPokemon(speciesName)
+
+    evolutions.push({
+      id: pokemonDetails.id,
+      name: pokemonDetails.name,
+      sprites: pokemonDetails.sprites.other?.['official-artwork'].front_default,
+      types: pokemonDetails.types,
+    })
+
+    if (chain.evolves_to.length > 0) {
+      for (const evolution of chain.evolves_to) {
+        await traverseEvolutions(evolution)
+      }
+    }
+  }
+  await traverseEvolutions(data.chain)
+
+  return evolutions
+}
+
+export const getEvolutions = cache(fetchEvolutions)
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,47 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+/**
+ * This function formats an id number to a string with leading zeros.
+ * @param id - The number to be formatted.
+ * @returns A string representation of the input number with leading zeros.
+ *
+ * @example
+ * ```typescript
+ * const formattedId = formatterId(12); // formattedId = "0012"
+ * ```
+ */
+export function formatterId(id: number): string {
+  return String(id).padStart(4, '0')
+}
+/**
+ * This function formats a name string to have the first letter capitalized.
+ * @param name - The string to be formatted.
+ * @returns A string representation of the input name with the first letter capitalized.
+ *
+ * @example
+ * ```typescript
+ * const formattedName = formatterName('john'); // formattedName = "John"
+ * ```
+ */
+export function formatterName(name: string): string {
+  return name.charAt(0).toUpperCase() + name.slice(1)
+}
+
+/**
+ * This function formats a height number to a decimal value.
+ * @param height - The number to be formatted. It should be a whole number representing the height in centimeters.
+ * @returns A decimal representation of the input height. The returned value is in meters.
+ */
+export function formattedHeight(height: number): number {
+  return height * 10
+}
+/**
+ * This function formats a weight number to a decimal value.
+ * @param weight - The number to be formatted.
+ * @returns A decimal representation of the input weight.
+ */
+export function formattedWeight(weight: number): number {
+  return weight / 10
+}
