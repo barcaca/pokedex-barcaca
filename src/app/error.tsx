dfeec3c80682a181f31902097ea51671ae754ee0
barcaca <73846225+barcaca@users.noreply.github.com>
2024-06-22 15:57:43 -0300

    feat: adiciona error.tsx para gerenciamento de erros

---
+++ src/app/error.tsx
@@ -0,0 +1,27 @@
+'use client' // Error components must be Client Components
+
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
+
+import { Button } from '@/components/ui/button'
+
+export default function Error({
+  error,
+}: {
+  error: Error & { digest?: string }
+}) {
+  const router = useRouter()
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error)
+  }, [error])
+
+  return (
+    <div className="mx-auto flex max-w-screen-sm flex-col items-center gap-8">
+      <h2>Something went wrong!</h2>
+      <Button variant={'outline'} onClick={() => router.back()}>
+        Back to Home
+      </Button>
+    </div>
+  )
+}
